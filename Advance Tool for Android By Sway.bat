57e0b5912172cc04c35187a9820dd7077021c155
Max <56435388+SwayWasTaken@users.noreply.github.com>
2019-10-22 01:00:52 +0200

    New Version V0.3
    Some changes in the design

--- Advance Tool for Android By Sway.bat
+++ Advance Tool for Android By Sway.bat
@@ -1,13 +1,13 @@
 @echo off
 TITLE ATA Tool by Sway
 
-ECHO      ######  ##               ##    ######  ##   ## 
-ECHO     ##       ##   ###  ###   ##   ##   ##   ## ## 
-ECHO     ######   ##   ##  ##   ##    #######    ### 
-ECHO       ##     ##   ####   ##    ##   ##     ## 
+ECHO      ######  ##               ##    ######  ##   ##
+ECHO     ##       ##   ###  ###   ##   ##   ##   ## ##
+ECHO     ######   ##   ##  ##   ##    #######    ###
+ECHO       ##     ##   ####   ##    ##   ##     ##
 ECHO  ######      #####  #####    ##   ##     ##
 ECHO 	::::::::::::::::::::::::::::::::
-ECHO  	:: ATA Tool V0.2              ::
+ECHO  	:: ATA Tool V0.3              ::
 ECHO  	:: adb and fastboot tool      ::
 ECHO  	:: Created By Sway	      ::
 ECHO  	:: Copyright 2019 Sway	      ::
@@ -19,7 +19,7 @@ echo a >> settings.txt
 :prelaunch
 for /f %%a in (settings.txt) do (
 if %%a==827361536237637183256253671867278168 (
-goto :menu 
+goto :menu
 ) else (
 goto :disclaimer
 )
@@ -28,10 +28,10 @@ goto :disclaimer
 
 :disclaimer
 echo *** Disclaimer ***
-echo I am not responsible for any damage you made to your device
+echo I am not responsible for any damage YOU will do to your device
 echo You have been warned, Do you accept? (Y/N)
 echo.
-SET /P inputd=Please Select: 
+SET /P inputd=Please Select:
 if %inputd%==Y (
 SET inputd=827361536237637183256253671867278168
 ) else (
@@ -45,20 +45,27 @@ if %inputd%==546s578eehds7s6553445401x exit
 :menu
 cls
 echo ========================================================
+echo DEVICE INFO
+echo ========================================================
+echo Device in Fastboot mode:
+fastboot devices 
 echo.
-echo Main Menu
-echo.
+adb devices
+echo Device Version: 
+adb shell getprop ro.build.version.release
+echo ========================================================
+echo MAIN MENU
+echo ========================================================
 echo 1.  Fastboot/Bootloader Commands
 echo.
-echo 2.  Recovery Commands (Sideload) 
+echo 2.  Recovery Commands (Sideload)
 echo.
 echo 3.  System Commands
-echo.
 echo ========================================================
-echo 0) EXIT 
+echo 0) EXIT
 echo ========================================================
 
-SET /P inputmm=Please Select: 
+SET /P inputmm=Please Select:
 
 if %inputmm%==1 goto menubootloader
 if %inputmm%==2 goto menurecovery
@@ -73,10 +80,10 @@ echo.
 echo What do you want to do?
 echo.
 echo 1) UNLOCK BOOTLOADER
-echo. 
+echo.
 echo 2) FORMAT DATA PARTITION (EXT4)
 echo.
-echo 3) FORMAT DATA PARTITION (F2FS) 
+echo 3) FORMAT DATA PARTITION (F2FS)
 echo.
 echo 4) FLASH RECOVERY (only .img file)
 echo.
@@ -91,24 +98,24 @@ echo.
 echo 9) Boot into recovery
 echo.
 echo ========================================================
-echo 0) EXIT 
+echo 0) EXIT
 echo ========================================================
 echo.
 SET /P inputmb=Please Select:
-  
-if %inputmb%==0 goto menu 
 
-if %inputmb%==1 fastboot oem device-info && fastboot oem unlock && echo checking. && cls && echo checking.. && cls && echo checking... && fastboot getvar unlocked 
+if %inputmb%==0 goto menu
+
+if %inputmb%==1 fastboot oem device-info && fastboot oem unlock && echo checking. && cls && echo checking.. && cls && echo checking... && fastboot getvar unlocked
 
 if %inputmb%==2 fastboot format:ext4 userdata
 
-if %inputmb%==3 fastboot format:f2fs userdata 
+if %inputmb%==3 fastboot format:f2fs userdata
 
-if %inputmb%==4 SET /P recovery=Write recovery name like (recoveryname.img) && fastboot flash recovery %recovery% 
+if %inputmb%==4 SET /P recovery=Write recovery name like (recoveryname.img) && fastboot flash recovery %recovery%
 
-if %inputmb%==5 SET /P kernel=Write kernel name like (kernelname.img) && fastboot flash boot %kernel% 
+if %inputmb%==5 SET /P kernel=Write kernel name like (kernelname.img) && fastboot flash boot %kernel%
 
-if %inputmb%==6 SET /P rom=Write rom path like (/path/to/your/Rom.zip) && fastboot -w && fastboot update %rom% 
+if %inputmb%==6 SET /P rom=Write rom path like (/path/to/your/Rom.zip) && fastboot -w && fastboot update %rom%
 
 if %inputmb%==7 fastboot devices
 
@@ -117,7 +124,7 @@ if %inputmb%==8 fastboot reboot
 if %inputmb%==9 fastboot reboot recovery
 
 pause
-goto menubootloader 
+goto menubootloader
 
 
 
@@ -127,15 +134,15 @@ echo ========================================================
 echo.
 echo What do you want to do?
 echo.
-echo 1) REBOOT SMARTPHONE 
-echo. 
+echo 1) REBOOT SMARTPHONE
+echo.
 echo 2) REBOOT INTO THE RECOVERY
 echo.
 echo 3) REBOOT INTO THE Fastboot/Bootloader
 echo.
 echo 4) Check connected devices
 echo.
-echo 5) Check devices version connected 
+echo 5) Check devices version connected
 echo.
 echo 6) Install an app
 echo.
@@ -148,22 +155,22 @@ echo.
 echo 10) Reset (Emulate device)
 echo.
 echo ========================================================
-echo 0) EXIT 
+echo 0) EXIT
 echo ========================================================
 echo.
 SET /P inputmb=Please Select:
-  
-if %inputmb%==0 goto menu 
+
+if %inputmb%==0 goto menu
 
 if %inputmb%==1 adb reboot
 
-if %inputmb%==2 adb reboot recovery 
+if %inputmb%==2 adb reboot recovery
 
 if %inputmb%==3 adb reboot-bootloader
 
 if %inputmb%==4 adb devices
 
-if %inputmb%==5 adb shell getprop ro.build.version.release 
+if %inputmb%==5 adb shell getprop ro.build.version.release
 
 if %inputmb%==6 SET /P i=Write the app name like (app.apk) && adb install -r %i%
 
@@ -184,15 +191,15 @@ echo ========================================================
 echo.
 echo What do you want to do?
 echo.
-echo 1) Sideload a zip 
-echo. 
+echo 1) Sideload a zip
+echo.
 echo ========================================================
-echo 0) EXIT 
+echo 0) EXIT
 echo ========================================================
 echo.
 SET /P inputmb=Please Select:
 
-if %inputmb%==0 goto menu 
+if %inputmb%==0 goto menu
 
 if %inputmb%==1 SET /P slzip=Write the zip name like (name.zip) && adb sideload %slzip%
 pause

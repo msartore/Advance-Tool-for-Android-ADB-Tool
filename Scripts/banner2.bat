34f25ed5fd3ba3dc6b2ac8dec315721e823a34d2
Max <56435388+MassimilianoSartore@users.noreply.github.com>
2020-01-12 13:34:11 +0100

    Add files via upload

--- Scripts/banner2.bat
+++ Scripts/banner2.bat
@@ -6,7 +6,7 @@ ECHO     ######   ##   ##  ##   ##    #######    ###
 ECHO       ##     ##   ####   ##    ##   ##     ##
 ECHO  ######      #####  #####    ##   ##     ##
 ECHO 	::::::::::::::::::::::::::::::::
-ECHO  	:: ATA Tool V0.5.9            ::
+ECHO  	:: ATA Tool V0.6.0            ::
 ECHO  	:: adb and fastboot tool      ::
 ECHO  	:: Created By Sway	      ::
 ECHO  	:: Copyright 2019 Sway	      ::

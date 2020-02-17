61786e7f1125b801f614dd4b0fd54a5f38473bc2
Max <56435388+MassimilianoSartore@users.noreply.github.com>
2020-02-17 23:09:23 +0100

    Update banner1.bat

--- Scripts/banner1.bat
+++ Scripts/banner1.bat
@@ -6,7 +6,7 @@ ECHO     ######   ##   ##  ##   ##    #######    ###
 ECHO       ##     ##   ####   ##    ##   ##     ##
 ECHO  ######      #####  #####    ##   ##     ##
 ECHO 	::::::::::::::::::::::::::::::::
-ECHO  	:: ATA Tool V0.6.9            ::
+ECHO  	:: ATA Tool V0.6.9.1          ::
 ECHO  	:: adb and fastboot tool      ::
 ECHO  	:: Created By Sway	      ::
 ECHO  	:: Copyright 2019 Sway	      ::

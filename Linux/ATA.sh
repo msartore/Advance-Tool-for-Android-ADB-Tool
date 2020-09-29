15d974e7dfe838aca6fd16df013491ae9fd0d7f9
Massimiliano Sartore <massimiliano.sartore@protonmail.com>
2020-09-30 00:24:09 +0200

    Add ATA check integrity and disclaimer, Remove adb and fastboot

--- Linux/ATA.sh
+++ Linux/ATA.sh
@@ -1,4 +1,68 @@
 #!/bin/bash
+mytitle="ATA Tool by Sway"
+echo -e '\033]2;'$mytitle'\007'
+# Tool made by Sway 
+# github repository: https://github.com/MassimilianoSartore/Advance-Tool-for-Android-ADB-Tool-
+# twitter: https://twitter.com/SWayWasTaken
+# email: massimiliano.sartore@protonmail.com
+# Copyright (C) 2019-2020 SwayWasTaken
+
+checkdisclaimer(){
+    if [ ! -f "settings.ini" ]; then
+        touch settings.ini
+    fi
+    if  grep -q "disclaimer:417070726f766564" "settings.ini" ; then
+        checkintegrity
+    else
+        disclaimer
+    fi
+}
+
+checkintegrity(){
+    if [ -f "adb" ] && [ -f "fastboot" ]; then
+        mainmenu
+    else
+        echo "Error, Something went wrong ATA is trying to repair itself"
+        wget -q --spider http://google.com
+        if [ $? -eq 0 ]; then
+            echo "platform-tools download started"
+            wget -O adb.zip https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+            echo "Unzipping adb.zip cd "
+            unzip adb.zip
+            echo "Moving fastboot and adb in $PWD"
+            mv $PWD/platform-tools/fastboot $PWD
+            mv $PWD/platform-tools/adb $PWD
+            echo "Deleting platform-tools folder"
+            
+            rm -rf platform-tools
+            echo "Deleting adb.zip"
+            rm adb.zip    
+            checkintegrity 
+        else
+            echo "Error [connection not available]"
+            exit
+        fi  
+    fi
+}
+
+disclaimer(){
+    reset
+    banner
+    echo "*** Disclaimer ***"
+    echo "- ATA is able to repair itself[Missing Files], if you accept this Disclaimer ATA will repair itself if a connection is available"
+    echo "- I am not responsible for any damage YOU will do to your device"
+    echo "You have been warned, Do you accept? (Y/N)"
+    echo  "       "
+    echo  "Please Select:"
+    read inputdisclaimer
+    if [ "$inputdisclaimer" == "Y"  ] || [ "$inputdisclaimer" == "y" ]; then
+        echo "disclaimer:417070726f766564" > settings.ini
+    else
+        echo "disclaimer:6e6f7420617070726f766564" > settings.ini
+    fi
+    checkdisclaimer
+}
+
 pause(){
     echo  "Press [Enter] key to continue..."
 	read -p "$*"
@@ -52,7 +116,7 @@ mainRecovery(){
         ;;
     esac
     echo  "Press [Enter] key to continue..."
-    read 1 -p prompt
+    pause
     mainRecovery
 }
 
@@ -164,113 +228,113 @@ mainFastboot()
 
 mainSystem()
 {
-reset 
-banner
-echo  "================================================================================="
-echo  " SYSTEM MENU "
-echo  "================================================================================="
-echo  " 1. Check connected devices "
-echo  " 2. REBOOT SMARTPHONE "
-echo  " 3. REBOOT INTO THE RECOVERY "
-echo  " 4. REBOOT INTO THE Fastboot/Bootloader "
-echo  " 5. Device version "
-echo  " 6. Device serial number "
-echo  " 7. Enable Dark Mode "
-echo  " 8. Disable Dark Mode "
-echo  " 9. APK MENU  "
-echo  "================================================================================="
-echo  " 0. Return to mainSystem menu"
-echo  "================================================================================="
+    reset 
+    banner
+    echo  "================================================================================="
+    echo  " SYSTEM MENU "
+    echo  "================================================================================="
+    echo  " 1. Check connected devices "
+    echo  " 2. REBOOT SMARTPHONE "
+    echo  " 3. REBOOT INTO THE RECOVERY "
+    echo  " 4. REBOOT INTO THE Fastboot/Bootloader "
+    echo  " 5. Device version "
+    echo  " 6. Device serial number "
+    echo  " 7. Enable Dark Mode "
+    echo  " 8. Disable Dark Mode "
+    echo  " 9. APK MENU  "
+    echo  "================================================================================="
+    echo  " 0. Return to mainSystem menu"
+    echo  "================================================================================="
 
 
-echo  "Please Select:"
-read inputmenu
+    echo  "Please Select:"
+    read inputmenu
 
-case $inputmenu in
+    case $inputmenu in
 
-   0)
-    mainmenu
-    ;;
-   1)
-    ./adb devices
-    ;;
-   2)
-    ./adb reboot
-    ;;
-   3)
-    ./adb reboot recovery
-    ;;
-   4)
-    ./adb reboot-bootloader
-    ;;
-   5)
-    ./adb shell getprop ro.build.version.release
-    ;;
-   6)
-    ./adb get-serialno
-    ;;
-   7)
-    ./adb shell settings put secure ui_night_mode 2
-    ./adb reboot
-    ;;
-   8)
-    ./adb shell settings put secure ui_night_mode 1
-    ./adb reboot
-    ;;
-   9)
-    APKmenu
-    ;;
-esac
-pause
-mainSystem
+    0)
+        mainmenu
+        ;;
+    1)
+        ./adb devices
+        ;;
+    2)
+        ./adb reboot
+        ;;
+    3)
+        ./adb reboot recovery
+        ;;
+    4)
+        ./adb reboot-bootloader
+        ;;
+    5)
+        ./adb shell getprop ro.build.version.release
+        ;;
+    6)
+        ./adb get-serialno
+        ;;
+    7)
+        ./adb shell settings put secure ui_night_mode 2
+        ./adb reboot
+        ;;
+    8)
+        ./adb shell settings put secure ui_night_mode 1
+        ./adb reboot
+        ;;
+    9)
+        APKmenu
+        ;;
+    esac
+    pause
+    mainSystem
 }
 
 APKmenu()
 {
-reset 
-banner
-echo  "================================================================================="
-echo  " APK MENU "
-echo  "================================================================================="
-echo "1) List of non system app"
-echo "2) List of system app"
-echo "3) Install an app"
-echo "4) Unistall an app (No System App)"
-echo "5) Unistall System App/Bloat "
-echo  "================================================================================="
-echo  " 0. Return to mainSystem menu"
-echo  "================================================================================="
-echo  "Please Select:"
-read inputmenuapk
+    reset 
+    banner
+    echo  "================================================================================="
+    echo  " APK MENU "
+    echo  "================================================================================="
+    echo "1) List of non system app"
+    echo "2) List of system app"
+    echo "3) Install an app"
+    echo "4) Unistall an app (No System App)"
+    echo "5) Unistall System App/Bloat "
+    echo  "================================================================================="
+    echo  " 0. Return to mainSystem menu"
+    echo  "================================================================================="
+    echo  "Please Select:"
+    read inputmenuapk
 
-case $inputmenuapk in
-   
-   0)
-    mainSystem
-    ;;
-   1)
-    ./adb shell pm list packages -3
-    ;;
-   2)
-    ./adb shell pm list packages -s
-    ;;
-   3)
-   echo  "Write the app name like (com.myAppPackage)"
-   read inputapkname
-    ./adb install -r $inputapkname
-    ;;
-   4)
-   echo  "Write the app name like (com.myAppPackage)"
-   read inputapkname
-    ./adb uninstall $inputapkname
-    ;;
-   5)
-   echo  "Write the app name like (com.myAppPackage)"
-   read inputapkname
-    ./adb shell pm uninstall -k --user 0 $inputapkname
-    ;;
-esac
-pause
-APKmenu
+    case $inputmenuapk in
+    
+    0)
+        mainSystem
+        ;;
+    1)
+        ./adb shell pm list packages -3
+        ;;
+    2)
+        ./adb shell pm list packages -s
+        ;;
+    3)
+    echo  "Write the app name like (com.myAppPackage)"
+    read inputapkname
+        ./adb install -r $inputapkname
+        ;;
+    4)
+    echo  "Write the app name like (com.myAppPackage)"
+    read inputapkname
+        ./adb uninstall $inputapkname
+        ;;
+    5)
+    echo  "Write the app name like (com.myAppPackage)"
+    read inputapkname
+        ./adb shell pm uninstall -k --user 0 $inputapkname
+        ;;
+    esac
+    pause
+    APKmenu
 }
-mainmenu
+checkdisclaimer
